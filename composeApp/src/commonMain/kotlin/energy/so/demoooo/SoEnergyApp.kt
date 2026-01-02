6441c74fb2fb81f1d75050ffff51f935f5eb8045
Samuel Patmore <sam.patmore@so.energy>
2026-01-02 11:47:22 +0000

    Create basic page structure

--- composeApp/src/commonMain/kotlin/energy/so/demoooo/SoEnergyApp.kt
+++ composeApp/src/commonMain/kotlin/energy/so/demoooo/SoEnergyApp.kt
@@ -2,15 +2,12 @@ package energy.so.demoooo
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -49,23 +46,42 @@ fun Body() {
                 .height(400.dp),
             text = "Video section",
         )
-        Text(
-            modifier = Modifier.background(Color.Red)
-                .fillMaxWidth()
-                .height(400.dp),
-            text = "common row component 1"
+        CommonTitleSubtitleRowComponent(
+            modifier = Modifier.background(Color.Red),
+            title = "What we do",
+            subtitle = "We're all about making life easier for you.\n" +
+                    "Simple solutions for your energy needs.",
         )
-        Text(
-            modifier = Modifier.background(Color.DarkGray)
-                .fillMaxWidth()
-                .height(400.dp),
-            text = "common row component 2"
+        CommonTitleSubtitleRowComponent(
+            modifier = Modifier.background(Color.DarkGray),
+            title = "Why So Energy?",
+            subtitle = "Energy doesn't have to be complicated. From honest pricing to UK-based customer support, we make switching, saving, and being sustainable simple.",
         )
-        Text(
-            modifier = Modifier.background(Color.Magenta)
-                .fillMaxWidth()
-                .height(400.dp),
-            text = "common row component 3"
+        CommonTitleSubtitleRowComponent(
+            modifier = Modifier.background(Color.Magenta),
+            title = "common row component 3",
+            subtitle = "common row component 3",
         )
     }
 }
+
+@Composable
+private fun CommonTitleSubtitleRowComponent(
+    modifier: Modifier = Modifier,
+    title: String,
+    subtitle: String,
+) {
+    Text(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(40.dp),
+        text = title
+    )
+    Text(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(40.dp),
+        text = subtitle
+    )
+}
+
